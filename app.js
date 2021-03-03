2830db630e110c3c9cfd7318ee73543a80c20f2f
Olivier-BeCode <76862461+Olivier-BeCode@users.noreply.github.com>
2021-03-03 08:55:43 +0100

    Night and background

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const token = 'a76d54bcbabc1f1ebc1f6eaea18f6ec7';
 const city = document.getElementById('City');
 const temp = document.getElementById('Temp');
 const time = document.getElementById('Time');
+const wind = document.getElementById('Wind');
 
 
 if(navigator.geolocation) {
@@ -23,9 +24,22 @@ function APICall(long, lat) {
     })
     .then((data) => {
         console.log(data);
-        
+
         city.innerText = data.timezone;
         temp.innerText = `${(data.current.temp)}°`;
+        wind.innerText = `${parseInt((data.current.wind_speed)*3.6)} km/h`;
+
+        let night;
+        Math.round(new Date() / 1000) >= data.current.sunset && Math.round(new Date() / 1000) <= data.current.sunrise ? night = 1 : night = 0;
+        // console.log(night);
+        if (night === 1)
+        {
+            document.body.style.backgroundImage = "url(./Assets/IMG/night.jfif)";
+        }
+        else if (night === 0)
+        {
+            document.body.style.backgroundImage = "url(./Assets/IMG/day.jpeg)";
+        }
 
     })
 }
